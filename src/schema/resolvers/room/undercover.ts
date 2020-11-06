64402912395c9918cc86d42f6f3f932188c1fe69
Colin <colinwang.0616@gmail.com>
2020-11-06 17:42:28 +0800

    feat(undercover): add custom create room api
    use another join

--- src/schema/resolvers/room/undercover.ts
+++ src/schema/resolvers/room/undercover.ts
@@ -1,18 +1,42 @@
-import { Resolver } from 'type-graphql';
+import { Resolver, Mutation, Arg } from 'type-graphql';
 import { Inject, Service } from 'typedi';
 
 import RoomService, { RoomServiceName } from '@services/room';
 import { UnderCoverRoom, UnderCoverRoomModelName } from '@models/undercover/room';
+import { IRoom } from '@models/room';
+import { InputGameConfig } from '@input/gameConfig';
+import logger from '@shared/Logger';
 
 import createAbstractRoom from './base';
 
-const UnderCoverBaseResolver = createAbstractRoom('undercover', UnderCoverRoom);
+const prefix = 'undercover';
+
+const UnderCoverBaseResolver = createAbstractRoom(prefix, UnderCoverRoom);
 
 @Service()
 @Resolver(() => UnderCoverRoom)
 class UnderCover extends UnderCoverBaseResolver {
   @Inject(`${UnderCoverRoomModelName}${RoomServiceName}`)
   protected readonly service!: RoomService;
+
+  @Mutation(() => UnderCoverRoom, {
+    name: `${prefix}CreateRoom`,
+    description: '创建房间',
+  })
+  async createRoom(@Arg('config') config: InputGameConfig): Promise<IRoom> {
+    const room = await this.service.createRoom(config);
+
+    if (!room || !room.roomNumber) {
+      logger.error('创建房间失败，模版 %s', config);
+      throw new Error('创建房间失败');
+    }
+
+    await this.service.joinRoom(room.roomNumber);
+
+    logger.info('create room with number: %s', room.roomNumber);
+
+    return room;
+  }
 }
 
 export default UnderCover;
